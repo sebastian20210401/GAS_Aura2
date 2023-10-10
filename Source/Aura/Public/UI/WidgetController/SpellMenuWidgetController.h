e36a549196f52f95f5df8c1088e8fb898c2e9568
sebastian20210401 <sebastian20210401@gmail.com>
2023-10-10 17:47:24 +0800

    Section 24-277
    都在做UI

---
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API USpellMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
